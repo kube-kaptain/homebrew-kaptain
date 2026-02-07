ce88b47d549c9ce0385ecfe00fd972383689cf41
Fred Cooke <fred.cooke@gmail.com>
2026-02-07 22:57:10 +1300

    Fix the metapackage and new keygen script.

--- Formula/kaptain-user-scripts.rb
+++ Formula/kaptain-user-scripts.rb
@@ -5,7 +5,7 @@ class KaptainUserScripts < Formula
   homepage "https://github.com/kube-kaptain/kaptain-user-scripts"
   version KAPTAIN_VERSION
   url "https://github.com/kube-kaptain/kaptain-user-scripts/releases/download/#{version}/kaptain-user-scripts-#{version}.zip"
-  sha256 "1ccba03e69dc9414c90fb2dd206d988eca09cf67c706a4f6ca0aad621191e37e"
+  sha256 "2abf47348129a6fe14d2f251644bd29196bfaba0cf36ee77df90595c8970d046"
   license "MIT"
 
   depends_on "age"
