a963723043084b32cb92600ff3f1b248726e39fa
Fred Cooke <fred.cooke@gmail.com>
2026-03-03 22:41:44 +1300

    Upgrade to 1.0.11 user scripts with list config improved list secrets improved keygen added bash completion and probably more. Docs in the up stream repo are much better too.

--- Formula/kaptain-encryption.rb
+++ Formula/kaptain-encryption.rb
@@ -5,7 +5,7 @@ class KaptainEncryption < Formula
   homepage "https://github.com/kube-kaptain/kaptain-user-scripts"
   version KAPTAIN_VERSION
   url "https://github.com/kube-kaptain/kaptain-user-scripts/releases/download/#{version}/kaptain-user-scripts-encryption-#{version}.zip"
-  sha256 "d7ece74deeebc452c6d3ed28cba03d0f094e0b66f801ccd1de79f119d2094f5a"
+  sha256 "49d1d80a7c67206d4a9c5c190149f25c6103ea99a9f6f51d2e6783b97d894028"
   license "MIT"
 
   depends_on "age"
